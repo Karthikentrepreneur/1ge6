6e4a8598b5c3118e1448aef5fdc6c91a823efa62
Karthikentrepreneur <karthikjungleemara@gmail.com>
2025-09-08 20:41:52 +0530

    Offset content after hero with reusable class

--- src/Pages/Home3.jsx
+++ src/Pages/Home3.jsx
@@ -1,15 +1,7 @@
 import React from 'react';
 import Heroanner1 from '../Components/HeroBanner/Heroanner1';
-import Feature1 from '../Components/Feature/Feature1';
 import Category1 from '../Components/Category/Category1';
 import About1 from '../Components/About/About1';
-import Cta1 from '../Components/Cta/Cta1';
-import Destination1 from '../Components/Destination/Destination1';
-import DealOffers1 from '../Components/DealOffers/DealOffers1';
-import Testimonial1 from '../Components/Testimonial/Testimonial1';
-import Choose1 from '../Components/Choose/Choose1';
-import Team1 from '../Components/Team/Team1';
-import Cta2 from '../Components/Cta/Cta2';
 import Blog1 from '../Components/Blog/Blog1';
 import Brand1 from '../Components/Brand/Brand1';
 
@@ -17,7 +9,7 @@ const Home3 = () => {
     return (
         <div>
             <Heroanner1></Heroanner1>
-            <div style={{ marginTop: '-140px', paddingTop: '140px' }}>
+            <div className="after-hero-offset">
                 <About1></About1>
                 <Category1></Category1>
                 <Blog1></Blog1>
