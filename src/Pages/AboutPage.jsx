14f859f6178b07686212225cd5d55733610d2a69
Karthikentrepreneur <karthikjungleemara@gmail.com>
2025-10-08 03:38:00 +0530

    Update AboutPage.jsx

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import BreadCumb from '../Components/Common/BreadCumb';
 import AboutSimple from '../Components/About/AboutSimple';
+import AboutLeadership from '../Components/About/AboutLeadership';
 import Choose1 from '../Components/Choose/Choose1';
 import Counter4 from '../Components/Counter/Counter4';
 import Team1 from '../Components/Team/Team1';
