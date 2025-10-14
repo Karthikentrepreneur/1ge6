f381c4ce5abcbe80ae9114d1288f94d7c4cee539
Karthikentrepreneur <karthikjungleemara@gmail.com>
2025-10-14 11:52:53 +0530

    Update About1.jsx

--- src/Components/About/About1.jsx
+++ src/Components/About/About1.jsx
@@ -188,7 +188,7 @@ const About1 = () => {
             <div className="col-lg-7 d-flex justify-content-center">
               <div className="about-photo-wrap">
                 <img
-                  src="/team.jpeg"
+                  src="/team.jpg"
                   alt="1 Global Enterprises Group"
                   className="about-photo"
                 />
