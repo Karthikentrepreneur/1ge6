101f21173f54f7542b22f3a9e5620a24507624af
Karthikentrepreneur <karthikjungleemara@gmail.com>
2025-10-14 14:17:18 +0530

    Update Heroanner1.jsx

--- src/Components/HeroBanner/Heroanner1.jsx
+++ src/Components/HeroBanner/Heroanner1.jsx
@@ -93,7 +93,7 @@ const Heroanner1 = () => {
         {/* Background video */}
         <div className="video-bg" aria-hidden="true">
           <video
-            src="/video3.mp4"
+            src="/video4.mp4"
             autoPlay
             muted
             loop
