8ffc135e3b13240d7f3770e260c5f1b11b6d8703
Karthikentrepreneur <karthikjungleemara@gmail.com>
2025-10-30 13:11:14 +0530

    Update countriesData.js

--- src/Components/GlobalPresence/countriesData.js
+++ src/Components/GlobalPresence/countriesData.js
@@ -12,7 +12,6 @@ export const COUNTRIES = [
         address:
           "Town Center - 2, Office No. 607, 6th Floor, Marol, Andheri Kurla Road, Andheri East, Mumbai - 400059.",
         contacts: ["+91 8879756838", "022-35131688", "35113475", "35082586"],
-        email: "info@oecl.sg",
       },
       {
         name: "Delhi",
@@ -21,7 +20,6 @@ export const COUNTRIES = [
         address:
           "Plot No. 15, 1st Floor, Block C, Pocket 8, Sector 17, Dwarka, New Delhi 110075",
         contacts: ["+91 11 41088871"],
-        email: "info@oecl.sg",
       },
       {
         name: "Chennai Warehouse",
@@ -30,15 +28,14 @@ export const COUNTRIES = [
         address:
           "Survey No. 209/6A(Part) 209/6B(Part), Mannur & Valarpuram Village, Perambakkam Road, Sriperumbudur Taluk, Kanchipuram District - 602105",
         contacts: ["+91 9994355523"],
-        email: "info@oecl.sg",
       },
       {
         name: "Chennai",
         lat: 13.0068,
         lng: 80.2048,
-        address: "Roma Building, Door No. 10, 3rd Floor, G.S.T. Road, Alandur, Chennai - 600 016",
+        address:
+          "Roma Building, Door No. 10, 3rd Floor, G.S.T. Road, Alandur, Chennai - 600 016",
         contacts: ["044 4689 4646"],
-        email: "info@oecl.sg",
       },
       {
         name: "Kerala",
@@ -47,7 +44,6 @@ export const COUNTRIES = [
         address:
           "CC 59/801A Elizabeth Memorial Building, Thevara Ferry Jn, Cochin 682013, Kerala",
         contacts: ["+91 484 4019192", "+91 484 4019193"],
-        email: "info@oecl.sg",
       },
       {
         name: "Hyderabad",
@@ -56,7 +52,6 @@ export const COUNTRIES = [
         address:
           "H.No. 1-8-450/1/A-7 Indian Airlines Colony, Opp Police Lines, Begumpet Hyderabad - 500016, Telangana",
         contacts: ["040-49559704"],
-        email: "info@oecl.sg",
       },
       {
         name: "Bangalore",
@@ -65,7 +60,6 @@ export const COUNTRIES = [
         address:
           "3C-964 IIIrd Cross Street, HRBR Layout 1st Block, Kalyan Nagar Banaswadi, Bengaluru - 560043",
         contacts: ["+91 9841676259"],
-        email: "info@oecl.sg",
       },
       {
         name: "Kolkata",
@@ -74,7 +68,6 @@ export const COUNTRIES = [
         address:
           "Imagine Techpark, Unit No. 10, 19th Floor, Block DN 6, Sector V Salt Lake City, Kolkata, West Bengal, India - 700091",
         contacts: ["+91 33 4814 9162", "+91 33 4814 9163"],
-        email: "info@oecl.sg",
       },
     ],
   },
@@ -90,8 +83,12 @@ export const COUNTRIES = [
         lng: 103.7629,
         address:
           "Unit 20-03A, Level 20 Menara Zurich, 15 Jalan Dato Abdullah Tahir, 80300 Johor Bahru",
-        contacts: ["+603-3319 2778", "+603-3319 2774", "+603-3319 2775", "+603-3319 2779"],
-        email: "info@oecl.sg",
+        contacts: [
+          "+603-3319 2778",
+          "+603-3319 2774",
+          "+603-3319 2775",
+          "+603-3319 2779",
+        ],
       },
       {
         name: "Port Klang",
@@ -100,7 +97,6 @@ export const COUNTRIES = [
         address:
           "MTBBT 2, 3A-5, Jalan Batu Nilam 16, The Landmark (Behind AEON Mall), Bandar Bukit Tinggi 2, 41200 Klang, Selangor",
         contacts: ["+603-3319 2778", "+603-3319 2774", "+603-3319 2775"],
-        email: "info@oecl.sg",
       },
     ],
   },
@@ -145,7 +141,8 @@ export const COUNTRIES = [
         name: "Doha",
         lat: 25.276987,
         lng: 51.520008,
-        address: "Office No. 48, 2nd Floor, Al Matar Centre, Old Airport Road, Doha",
+        address:
+          "Office No. 48, 2nd Floor, Al Matar Centre, Old Airport Road, Doha",
         contacts: ["0974 33622555"],
       },
     ],
@@ -163,7 +160,6 @@ export const COUNTRIES = [
         address:
           "13C02, Block A, Zhaoxin Huijin Plaza, 3085 Shennan East Road, Luohu, Shenzhen",
         contacts: ["+86 755 8222 2447"],
-        email: "helen@haixun.co",
       },
     ],
   },
@@ -213,7 +209,6 @@ export const COUNTRIES = [
         address:
           "Blk 511 Kampong Bahru Road, #03-01 Keppel Distripark, Singapore - 099447",
         contacts: ["+65 6908 0838"],
-        email: "info@oecl.sg",
       },
     ],
   },
@@ -230,7 +225,6 @@ export const COUNTRIES = [
         address:
           "408, Lina Building, JL. HR Rasuna Said Kav B7, Jakarta",
         contacts: ["+62 21 529 20292", "+62 21 522 4887"],
-        email: "logistics.jkt@oecl.sg",
       },
       {
         name: "Surabaya",
@@ -239,7 +233,6 @@ export const COUNTRIES = [
         address:
           "Japfa Indoland Center, Japfa Tower 1, Lantai 4/401-A JL Jend, Basuki Rahmat 129-137, Surabaya 60271",
         contacts: ["+62 21 529 20292", "+62 21 522 4887"],
-        email: "logistics.jkt@oecl.sg",
       },
     ],
   },
@@ -256,7 +249,6 @@ export const COUNTRIES = [
         address:
           "Ceylinco House, 9th Floor, No. 69, Janadhipathi Mawatha, Colombo 01, Sri Lanka",
         contacts: ["+94 114 477 499", "+94 114 477 494", "+94 114 477 498"],
-        email: "info.cmb@globalconsol.com",
       },
     ],
   },
@@ -273,7 +265,6 @@ export const COUNTRIES = [
         address:
           "109 CCT Building, 3rd Floor, Room 3, Surawong Road, Suriyawongse, Bangrak, Bangkok 10500",
         contacts: ["+662-634-3240", "+662-634-3942"],
-        email: "info@oecl.sg",
       },
     ],
   },
@@ -290,7 +281,6 @@ export const COUNTRIES = [
         address:
           "Suite No. 301, 3rd Floor, Fortune Center, Shahrah-e-Faisal, Block 6, PECHS, Karachi, Pakistan",
         contacts: ["+92-300-8282511", "+92-21-34302281-5"],
-        email: "info.pk@globalconsol.com",
       },
       {
         name: "Lahore",
@@ -298,8 +288,11 @@ export const COUNTRIES = [
         lng: 74.3487,
         address:
           "Office #301, 3rd Floor, Gulberg Arcade Main Market, Gulberg 2, Lahore, Pakistan",
-        contacts: ["+92 42-35782306", "+92 42-35782307", "+92 42-35782308"],
-        email: "info.pk@globalconsol.com",
+        contacts: [
+          "+92 42-35782306",
+          "+92 42-35782307",
+          "+92 42-35782308",
+        ],
       },
     ],
   },
@@ -315,7 +308,6 @@ export const COUNTRIES = [
         lng: -87.7209,
         address: "939 W. North Avenue, Suite 750, Chicago, IL 60642",
         contacts: ["+1 847 254 7320"],
-        email: "info@gglusa.us",
       },
       {
         name: "New York",
@@ -324,7 +316,6 @@ export const COUNTRIES = [
         address:
           "New Jersey Branch, 33 Wood Avenue South Suite 600, Iselin, NJ 08830",
         contacts: ["+1 732 456 6780"],
-        email: "info@gglusa.us",
       },
       {
         name: "Los Angeles",
@@ -332,7 +323,6 @@ export const COUNTRIES = [
         lng: -118.2437,
         address: "2250 South Central Avenue, Compton, CA 90220",
         contacts: ["+1 310 928 3903"],
-        email: "info@gglusa.us",
       },
     ],
   },
@@ -365,7 +355,6 @@ export const COUNTRIES = [
         address:
           "Suite 5, 7-9 Mallet Road, Tullamarine, Victoria, 3043",
         contacts: ["+61 432 254 969", "+61 3 8820 5157"],
-        email: "info@gglaustralia.com",
       },
     ],
   },
