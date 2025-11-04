b95c231c26f39786ee9b8d574b9f33fa6b273e04
Karthikentrepreneur <karthikjungleemara@gmail.com>
2025-11-04 18:36:34 +0530

    Update ActivitiesPage.jsx

--- src/Pages/ActivitiesPage.jsx
+++ src/Pages/ActivitiesPage.jsx
@@ -4,18 +4,7 @@ import Activities from '../Components/Activities/Activities';
 
 const ActivitiesPage = () => {
   return (
-    <main>
-      {/* ✅ Breadcrumb Section */}
-<BreadCumb
-  bgimg="/activities.png"
-  Title="Our Business Verticals"
-/>
-
-
-      {/* ✅ Activities Section */}
-      <Activities />
-    </main>
-
+   
             <div>
             <BreadCumb
                 bgimg="/activities.png"
